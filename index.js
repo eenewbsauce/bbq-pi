6359a43dc7612572ab27bfa991aa62d448494e00
Ryan Tracey <ryan.m.tracey@gmail.com>
2018-04-04 22:26:54 -0400

    feat(lib): added math w convertTimeToFarenheit

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const Probe = require('./lib/thermometers').Probe;
+const { temperature } = require('./lib/math');
 
 const thermometer1 = new Probe(5);
 
 thermometer1
     .start()
-    .on(`value`, (value) => {
-        console.log(`therm 1: ${value}`);
+    .on(`value`, (time) => {
+        console.log(`temp 1: ${ temperature.convertTimeToFarenheit(time) } F`);
     });
 
 const thermometer2 = new Probe(6);
 
 thermometer2
     .start()
-    .on(`value`, (value) => {
-        console.log(`therm 2: ${value}`);
+    .on(`value`, (time) => {
+        console.log(`temp 2: ${ temperature.convertTimeToFarenheit(time) } F`);
     });
