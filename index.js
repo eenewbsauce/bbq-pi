ea2cf43db0e6553567a9ca552ce0c9bb3caf4cfb
Ryan Tracey <ryan.m.tracey@gmail.com>
2018-04-04 20:50:36 -0400

    feat(index): add second thermometer

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const thermometer1 = new Probe(5);
 
 thermometer1
     .start()
-    .on(`5:value`, (value) => {
-        console.log(value)
+    .on(`value`, (value) => {
+        console.log(`therm 1: ${value}`);
+    });
+
+const thermometer2 = new Probe(6);
+
+thermometer2
+    .start()
+    .on(`value`, (value) => {
+        console.log(`therm 2: ${value}`);
     });
