8e95969a23052d6f1c6f3dbf2c0a69247b40665f
Ryan Tracey <ryan.m.tracey@gmail.com>
2018-04-04 22:45:45 -0400

    probes back in order

--- index.js
+++ index.js
@@ -3,14 +3,6 @@
 const Probe = require('./lib/thermometers').Probe;
 const { temperature } = require('./lib/math');
 
-const thermometer2 = new Probe(6);
-
-thermometer2
-    .start()
-    .on(`value`, (time) => {
-        console.log(`temp 2: ${ temperature.convertTimeToFarenheit(time) } F`);
-    });
-
 const thermometer1 = new Probe(5);
 
 thermometer1
@@ -19,4 +11,11 @@ thermometer1
         console.log(`temp 1: ${ temperature.convertTimeToFarenheit(time) } F`);
     });
 
+const thermometer2 = new Probe(6);
+
+thermometer2
+    .start()
+    .on(`value`, (time) => {
+        console.log(`temp 2: ${ temperature.convertTimeToFarenheit(time) } F`);
+    });
 
